9d8b9737b14205f02344f0a8001306d19941f235
danielgtr808 <danielbaquini@gmail.com>
2021-12-14 16:25:08 -0300

    Added simple output

---
+++ src/models/ladder-element/output/simple.ts
@@ -0,0 +1,44 @@
+import Network from "../../network";
+import LadderCoordinates from "../ladder-coordinates";
+import LadderElement from "../ladder-element";
+
+class Output implements LadderElement {
+
+    changed: boolean = false;
+    readonly hasNoPropagationTime: boolean = true;
+    isActive: boolean = false;
+
+    private _input: boolean = false;
+    private _output: boolean = false;
+
+    constructor(public readonly coordinates: LadderCoordinates, public readonly id: number, public readonly network: Network) { }
+
+    get input(): boolean {
+        return this._input;
+    }
+
+    set input(value: boolean) {
+        if(this.input == value) return;
+        this._input = value;
+        this.changed = true;
+    }
+
+    get output(): boolean {
+        return this._output;
+    }
+
+    reset(): void {
+        this._input = false;
+        this.isActive = false;
+        this._output = false;
+    }
+
+    resolve(): void {
+        if(this.input == this.output) return;
+        this._output = this.input
+        this.changed = true;
+    }
+
+}
+
+export default Output
